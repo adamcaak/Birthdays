aff84163a5e1f3db96273436d4e0c3754e72ffdf
adamcaak <ladamczak1997@gmail.com>
2025-07-07 19:32:58 +0200

    Add new name with birthday

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -12,6 +12,9 @@ struct ContentView: View {
         Friend(name: "Tom", birthday: .now),
         Friend(name: "Henry", birthday: Date(timeIntervalSince1970: 0))
     ]
+    @State private var newName: String = ""
+    @State private var newDate = Date.now
+    
     var body: some View {
         NavigationStack {
             List(friends, id: \.name) { friend in
@@ -22,6 +25,26 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New birthday")
+                        .font(.headline)
+                    DatePicker(selection: $newDate, in: Date.distantPast...Date.now, displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newDate)
+                        friends.append(newFriend)
+                        
+                        newName = ""
+                        newDate = Date.now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
     }
 }
